cee166463a6718a375dab7a9b929e6b835057025
Bo Liu <bo.robotics@gmail.com>
2018-05-19 20:23:34 +0000

    new branch: test GPS

--- src/modules/sensors/rc_update.cpp
+++ src/modules/sensors/rc_update.cpp
@@ -259,7 +259,7 @@ RCUpdate::rc_poll(const ParameterHandles &parameter_handles)
 		struct rc_input_values rc_input;
 
 		orb_copy(ORB_ID(input_rc), _rc_sub, &rc_input);
-//		PX4_INFO("%f %f %f %f", (double) rc_input.values[0], (double) rc_input.values[1], (double) rc_input.values[2], (double) rc_input.values[3]);
+//		PX4_INFO("~~~input rc: %f %f %f",(double)rc_input.values[0],(double)rc_input.values[2],(double)rc_input.values[3]);
 
 		/* detect RC signal loss */
 		bool signal_lost;
@@ -289,6 +289,7 @@ RCUpdate::rc_poll(const ParameterHandles &parameter_handles)
 				}
 			}
 		}
+//		PX4_INFO("signal_lost %d",signal_lost);
 
 		unsigned channel_limit = rc_input.channel_count;
 
@@ -309,6 +310,7 @@ RCUpdate::rc_poll(const ParameterHandles &parameter_handles)
 			if (rc_input.values[i] > _parameters.max[i]) {
 				rc_input.values[i] = _parameters.max[i];
 			}
+//					PX4_INFO("input rc: %f %f %f",(double)rc_input.values[0],(double)rc_input.values[2],(double)rc_input.values[3]);
 
 			/*
 			 * 2) Scale around the mid point differently for lower and upper range.
@@ -346,7 +348,6 @@ RCUpdate::rc_poll(const ParameterHandles &parameter_handles)
 				_rc.channels[i] = 0.0f;
 			}
 		}
-//		PX4_INFO("%f %f %f %f", (double) rc_input.values[0], (double) rc_input.values[1], (double) rc_input.values[2], (double) rc_input.values[3]);
 
 		_rc.channel_count = rc_input.channel_count;
 		_rc.rssi = rc_input.rssi;
@@ -357,6 +358,7 @@ RCUpdate::rc_poll(const ParameterHandles &parameter_handles)
 		/* publish rc_channels topic even if signal is invalid, for debug */
 		int instance;
 		orb_publish_auto(ORB_ID(rc_channels), &_rc_pub, &_rc, &instance, ORB_PRIO_DEFAULT);
+//		PX4_INFO("remote control: %f %f %f %f", (double)_rc.channels[0], (double)_rc.channels[1], (double)_rc.channels[2] ,(double)_rc.channels[3]);
 
 		/* only publish manual control if the signal is still present and was present once */
 		if (!signal_lost && rc_input.timestamp_last_signal > 0) {
@@ -386,6 +388,7 @@ RCUpdate::rc_poll(const ParameterHandles &parameter_handles)
 			manual.x = math::constrain(_filter_pitch.apply(manual.x), -1.f, 1.f);
 			manual.r = math::constrain(_filter_yaw.apply(manual.r), -1.f, 1.f);
 			manual.z = math::constrain(_filter_throttle.apply(manual.z), 0.f, 1.f);
+//			PX4_INFO("manual control: %f %f %f %f", (double)manual.x, (double)manual.y, (double)manual.z ,(double)manual.r);
 
 			if (_parameters.rc_map_flightmode > 0) {
 
@@ -446,6 +449,7 @@ RCUpdate::rc_poll(const ParameterHandles &parameter_handles)
 			/* publish manual_control_setpoint topic */
 			orb_publish_auto(ORB_ID(manual_control_setpoint), &_manual_control_pub, &manual, &instance,
 					 ORB_PRIO_HIGH);
+//			PX4_INFO("manual control: %f %f %f %f", (double)manual.x, (double)manual.y, (double)manual.z ,(double)manual.r);
 
 			/* copy from mapped manual control to control group 3 */
 			struct actuator_controls_s actuator_group_3 = {};
