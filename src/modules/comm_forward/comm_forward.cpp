bcad32424c5cc1b3ea1258291e6830b21574a006
jedichen121 <chenjiyang1991@gmail.com>
2018-06-04 15:56:54 -0500

    trying to increase gps forwarding frequency to 20Hz

--- src/modules/comm_forward/comm_forward.cpp
+++ src/modules/comm_forward/comm_forward.cpp
@@ -164,11 +164,17 @@ void Comm_forward::run()
 	
 
     while (!should_exit()) {
-    	/* wait for up to 1000ms for data */
-		int pret = px4_poll(&fds[0], (sizeof(fds) / sizeof(fds[0])), 1000);
+    	/* wait for up to 50ms for data */
+		int pret = px4_poll(&fds[0], (sizeof(fds) / sizeof(fds[0])), 50);
 
 		if (pret == 0) {
-		// Let the loop run anyway, don't do `continue` here. 
+			// Let the loop run anyway, don't do `continue` here. 
+			// increase sending frequency
+			gps.time_usec = hrt_absolute_time();
+
+			mavlink_message_t msg;
+			mavlink_msg_hil_gps_encode_chan(1, 200, MAVLINK_COMM_0, &msg, &gps);
+			send_mavlink_hil_gps(&msg);
 
 		} else if (pret < 0) {
 			// this is undesirable but not much we can do - might want to flag unhappy status 
@@ -180,7 +186,7 @@ void Comm_forward::run()
 
 			orb_copy(ORB_ID(vehicle_gps_position), vehicle_pos_fd, &_vehicle_gps);
 
-			gps.time_usec = _vehicle_gps.time_utc_usec;
+			gps.time_usec = hrt_absolute_time();
 
 			gps.lat = _vehicle_gps.lat;
 			gps.lon = _vehicle_gps.lon;
@@ -240,74 +246,4 @@ int comm_forward_main(int argc, char *argv[])
 	return Comm_forward::main(argc, argv);
 }
 
-
-
-// int comm_forward_main(int argc, char *argv[])
-// {
-
-
-// 	struct vehicle_gps_position_s	_gps_pos;
-
-//     PX4_INFO("Hello Sky!");
-//     PX4_INFO("Hello command forward!");
-
-//     /* subscribe to sensor_combined topic */
-//     int vehicle_pos_fd = orb_subscribe(ORB_ID(vehicle_gps_position));
-//     /* limit the update rate to 2 Hz */
-//     orb_set_interval(vehicle_pos_fd, 500);
-
-
-//  //    // /* advertise attitude topic */
-//  //    // struct vehicle_attitude_s att;
-//  //    // memset(&att, 0, sizeof(att));
-//  //    // orb_advert_t att_pub = orb_advertise(ORB_ID(vehicle_attitude), &att);
-
-
-//     /* copy all topics first time */
-//     orb_copy(ORB_ID(vehicle_gps_position), vehicle_pos_fd, &_gps_pos);
-
-//     /* one could wait for multiple topics with this technique, just using one here */
-//     px4_pollfd_struct_t fds[1] = {};
-//     fds[0].fd = vehicle_pos_fd;
-//     fds[0].events = POLLIN;
-
-//     while (1) {
-//     	/* wait for up to 1000ms for data */
-// 		int pret = px4_poll(&fds[0], (sizeof(fds) / sizeof(fds[0])), 1000);
-
-// 		if (pret == 0) {
-// 			 Let the loop run anyway, don't do `continue` here. 
-
-// 		} else if (pret < 0) {
-// 			// this is undesirable but not much we can do - might want to flag unhappy status 
-// 			PX4_ERR("poll error %d, %d", pret, errno);
-// 			usleep(10000);
-// 			continue;
-
-// 		} else {
-
-// 			orb_copy(ORB_ID(vehicle_gps_position), vehicle_pos_fd, &_gps_pos);
-
-// 			_gps_pos.timestamp = hrt_absolute_time();
-// 			// _gps_pos.lat = gps.lat;
-// 			// _gps_pos.lon = gps.lon;
-// 			// _gps_pos.alt = gps.alt;
-// 			// _gps_pos.eph = (float)gps.eph * 1e-2f;
-// 			// _gps_pos.epv = (float)gps.epv * 1e-2f;
-// 			// _gps_pos.vel_m_s = (float)(gps.vel) / 100.0f;
-// 			// _gps_pos.vel_n_m_s = (float)(gps.vn) / 100.0f;
-// 			// _gps_pos.vel_e_m_s = (float)(gps.ve) / 100.0f;
-// 			// _gps_pos.vel_d_m_s = (float)(gps.vd) / 100.0f;
-// 			// _gps_pos.cog_rad = (float)(gps.cog) * 3.1415f / (100.0f * 180.0f);
-// 			// _gps_pos.fix_type = gps.fix_type;
-// 			// _gps_pos.satellites_used = gps.satellites_visible;
-
-// 			// timestamp_last = gps.timestamp;
-// 			PX4_INFO("lat is: %f", (double) _gps_pos.lat);
-
-// 		}
-// 	}
-
-//     return OK;
-// }
 
