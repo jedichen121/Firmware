6abee3473fa51b3a9bfec8a4b2bf165d8a0d2c18
Jiyang Chen <chenjiyang@Jiyangs-MacBook-Pro.local>
2018-07-27 15:36:50 -0500

    disabled pwm output

--- src/drivers/linux_pwm_out/linux_pwm_out.cpp
+++ src/drivers/linux_pwm_out/linux_pwm_out.cpp
@@ -364,8 +364,8 @@ void task_main(int argc, char *argv[])
 
 			if (updated) {
 				orb_copy(ORB_ID(actuator_dummy_outputs), _dummy_outputs_sub, &_dummy_outputs);
-//				PX4_INFO("dummy: %f %f %f %f", (double) _dummy_outputs.output[0], (double) _dummy_outputs.output[1], (double) _dummy_outputs.output[2], (double) _dummy_outputs.output[3]);
-//				PX4_INFO("host: %f %f %f %f", (double) _outputs.output[0], (double) _outputs.output[1], (double) _outputs.output[2], (double) _outputs.output[3]);
+				PX4_INFO("dummy: %f %f %f %f", (double) _dummy_outputs.output[0], (double) _dummy_outputs.output[1], (double) _dummy_outputs.output[2], (double) _dummy_outputs.output[3]);
+				PX4_INFO("host: %f %f %f %f", (double) _outputs.output[0], (double) _outputs.output[1], (double) _outputs.output[2], (double) _outputs.output[3]);
 
 				for (size_t i = 0; i < 16; i++)
 					_outputs.output[i] = _dummy_outputs.output[i];
@@ -420,10 +420,10 @@ void task_main(int argc, char *argv[])
 					pwm[i] = pwm_value;
 				}
 
-				pwm_out->send_output_pwm(pwm, _outputs.noutputs);
+				// pwm_out->send_output_pwm(pwm, _outputs.noutputs);
 
-			} else {
-				pwm_out->send_output_pwm(pwm, _outputs.noutputs);
+			// } else {
+				// pwm_out->send_output_pwm(pwm, _outputs.noutputs);
 			}
 
 			if (_outputs_pub != nullptr) {
